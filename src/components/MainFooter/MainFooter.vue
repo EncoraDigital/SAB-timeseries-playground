43a9ab5442f9f63917915e4ec4c8a5cf75360930
João Pedro S. G. Silva <jpsilva@daitan.com>
2021-05-21 10:14:39 -0300

    Update footer

--- src/components/MainFooter/MainFooter.vue
+++ src/components/MainFooter/MainFooter.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="footer">
     <div>©{{ new Date().getFullYear() }} Daitan Group</div>
-    <span>Built by Innovation Team</span>
+    <span>This tool was built by João Pedro São Gregorio Silva (Developer)
+      and Thalles Silva (Data Scientist) with the Innovation Team at Daitan.</span>
   </div>
 </template>
 
